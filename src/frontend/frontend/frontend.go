f02b16fe5f6f230a0bffd3adb1fd7b25442470db
gav1995 <gav1995@inbox.ru>
2018-10-13 13:34:50 +0300

    frontend done

--- src/frontend/frontend/frontend.go
+++ src/frontend/frontend/frontend.go
@@ -1,8 +1,8 @@
 package frontend
 
 import (
-	"time"
 	"sync"
+	"time"
 
 	rclient "router/client"
 	"router/router"
@@ -40,50 +40,73 @@ type Config struct {
 // Frontend is a frontend service.
 type Frontend struct {
 	sync.RWMutex
-	conf Config
-
+	conf  Config
+	nodes []storage.ServiceAddr
+	once  sync.Once
 }
 
 // New creates a new Frontend with a given cfg.
 //
 // New создает новый Frontend с данным cfg.
 func New(cfg Config) *Frontend {
-	return &Frontend{conf:cfg,}
+	return &Frontend{conf: cfg}
+}
+
+func (fe *Frontend) putDel(k storage.RecordID, do func(node storage.ServiceAddr) error) error {
+	fe.Lock()
+	defer fe.Unlock()
+	nodes, err := fe.conf.RC.NodesFind(fe.conf.Router, k)
+
+	if err != nil {
+		return err
+	}
+
+	if len(nodes) < storage.MinRedundancy {
+		return storage.ErrNotEnoughDaemons
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	errors := make(map[error]int)
+	var good int = 0
+	wg.Add(len(nodes))
+
+	for _, node := range nodes {
+		go func(node storage.ServiceAddr) {
+			defer wg.Done()
+			err1 := do(node)
+
+			mu.Lock()
+			defer mu.Unlock()
+			if err1 == nil {
+				good++
+			} else {
+				errors[err1]++
+			}
+
+		}(node)
+	}
+	wg.Wait()
+
+	for err, num := range errors {
+		if num >= storage.MinRedundancy {
+			return err
+		}
+	}
+
+	if good >= storage.MinRedundancy {
+		return nil
+	}
+
+	return storage.ErrQuorumNotReached
+
 }
 
 // Put an item to the storage if an item for the given key doesn't exist.
 // Returns error otherwise.
-//
-// Put -- добавить запись в хранилище, если запись для данного ключа
-// не существует. Иначе вернуть ошибку.
 func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
-	fe.Lock()
-	defer fe.Unlock()
-	nodes, err:=fe.conf.RC.NodesFind(fe.conf.Router,k)
-	if err != nil {
-		return err
-	}
-	var wg sync.WaitGroup
-	good := 0
-	var mu sync.Mutex
-	wg.Add(len(nodes))
-	for i := range(nodes){
-		go func(){
-			defer wg.Done()
-			err1 := fe.conf.NC.Put(nodes[i],k, d)
-			if(err1 != nil){
-				err = err1
-			} else {
-				mu.Lock()
-				good ++
-				mu.Unlock()
-			}
-			
-		}()
-	}
-
-	wg.Wait()
-	return err
+	return fe.putDel(k, func(node storage.ServiceAddr) error {
+		return fe.conf.NC.Put(node, k, d)
+	})
 }
 
 // Del an item from the storage if an item exists for the given key.
@@ -92,34 +115,9 @@ func (fe *Frontend) Put(k storage.RecordID, d []byte) error {
 // Del -- удалить запись из хранилища, если запись для данного ключа
 // существует. Иначе вернуть ошибку.
 func (fe *Frontend) Del(k storage.RecordID) error {
-	fe.Lock()
-	defer fe.Unlock()
-	nodes, err:=fe.conf.RC.NodesFind(fe.conf.Router,k)
-	if err != nil {
-		return err
-	}
-	var wg sync.WaitGroup
-	var good = 0
-	var mu sync.Mutex
-	wg.Add(len(nodes))
-	for i := range(nodes){
-		go func(){
-			defer wg.Done()
-			err1 := fe.conf.NC.Del(nodes[i],k)
-			if(err1 != nil){
-				err = err1
-			} else {
-				mu.Lock()
-				good ++
-				mu.Unlock()
-			}
-			
-		}()
-	}
-
-	wg.Wait()
-	
-	return err
+	return fe.putDel(k, func(node storage.ServiceAddr) error {
+		return fe.conf.NC.Del(node, k)
+	})
 }
 
 // Get an item from the storage if an item exists for the given key.
@@ -128,6 +126,50 @@ func (fe *Frontend) Del(k storage.RecordID) error {
 // Get -- получить запись из хранилища, если запись для данного ключа
 // существует. Иначе вернуть ошибку.
 func (fe *Frontend) Get(k storage.RecordID) ([]byte, error) {
-	// TODO: implement
-	return nil, nil
+	fe.once.Do(func() {
+		var err error
+		fe.nodes, err = fe.conf.RC.List(fe.conf.Router)
+		for err != nil {
+			time.Sleep(InitTimeout)
+			fe.nodes, err = fe.conf.RC.List(fe.conf.Router)
+		}
+	})
+
+	nodes := fe.conf.NF.NodesFind(k, fe.nodes)
+
+	if len(nodes) < storage.MinRedundancy {
+		return nil, storage.ErrNotEnoughDaemons
+	}
+
+	values := make(map[string]int)
+	err := make(map[error]int)
+
+	type pair struct {
+		val []byte
+		err error
+	}
+
+	ch := make(chan pair, len(nodes))
+	for _, node := range nodes {
+		go func(node storage.ServiceAddr) {
+			val, err := fe.conf.NC.Get(node, k)
+			ch <- pair{val, err}
+		}(node)
+	}
+	for i := 0; i < storage.ReplicationFactor; i++ {
+		ans := <-ch
+		if ans.err != nil {
+			err[ans.err]++
+			if err[ans.err]+1 == storage.ReplicationFactor {
+				return nil, ans.err
+			}
+		} else {
+			values[string(ans.val)]++
+			if values[string(ans.val)]+1 == storage.ReplicationFactor {
+				return ans.val, nil
+			}
+		}
+	}
+
+	return nil, storage.ErrQuorumNotReached
 }
